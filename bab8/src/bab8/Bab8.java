645a8f8a30c8cacf2f95f7dad61f59a3eeb5713b
Bima Sakti <probima20@gmail.com>
2025-08-05 13:58:48 +0700

    Add files via upload

---
+++ bab8/src/bab8/Bab8.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab8;
+
+/**
+ *
+ * @author HP EliteBook 840 G5
+ */
+public class Bab8 {
+
+    public static void main(String[] args) {
+for (int a = 0; a < 10; a++) {
+System.out.println("Halo Dunia");
+}
+    }    
+}
